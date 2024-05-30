c2b9bf872212cd41941532d078fd14d95bc1db8d
ryano0oceros <ryanfallon@microsoft.com>
2024-05-30 17:05:53 -0400

    first functional

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# personalmoviedb-projectrefuge
-SQLite relational database to manage a personal movie collection
+# SQLite Movie Database Project
+
+## Project Overview
+
+This project involves setting up a SQLite relational database to manage a personal movie collection. The database is populated with movie data downloaded from Northwestern University's archive of IMDb data files. The project is implemented in Go, utilizing a cgo-free port of SQLite for database operations.
+
+## Setting Up the SQLite Database
+
+1. Download and extract the movie data files from [Northwestern University's archive](https://arch.library.northwestern.edu/concern/datasets/3484zh40n?locale=en).
+2. Define the SQLite relational database schema to accommodate the extracted data.
+3. Implement a Go program that serves as a local database server for SQLite.
+4. Populate the SQLite database with the movie data.
+
+## Performing SQL Queries
+
+The SQLite database allows for various SQL queries to be performed, enabling users to search, filter, and manage their movie collection effectively. Examples of SQL queries include searching for movies by genre, year, or director.
+
+The sample code provided below demonstrates how to perform a simple SQL query to grab a specific row and count the number of movies from a particular year:
+
+```go
+db.QueryRow("SELECT * FROM movies WHERE id = 191243")
+...
+db.QueryRow("SELECT COUNT(*) FROM movies WHERE year = 1991")
+...
+```
+
+returns the following output:
+```bash
+ID: 191243, Name: Lion King 1, The, Year: 2004, Rank: 0.000000
+Total movies from 1991: 6065
+```
+
+## Using the Database
+
+The SQLite database allows for various SQL queries to be performed, enabling users to search, filter, and manage their movie collection. Beyond SQL queries, the Go application provides a user-friendly interface for interacting with the database. Users can easily add, update, or remove movies from their personal collection, as well as view detailed information about each movie.
+
+## Potential Enhancements and Personal Movie Recommendation System
+
+As a future enhancement, one could include a table for personal movie ratings and locations, thereby enriching the database with personal collection details. Additionally, the development of a personal movie recommendation system based on user ratings and preferences is a promising direction. This system could leverage machine learning algorithms to provide personalized movie recommendations. Automated web scraping could also be implemented to update the database with metadata (similarly to how Plex media server works) to include things like the movie poster.
+
+## Advantages of the Go Movie Application
+
+The Go movie application offers several advantages over IMDb alone. It provides
+- Personalized experience: users can manage their own movie collection and ratings
+- Offline access to movie data: which can be useful in areas with limited internet access
+- Security and privacy: from advertisers and platforms looking to monetize your personal data
+- Customization: solution can be expanded upon to meet individual user needs
+
+## Conclusion
+
+The SQLite movie database project demonstrates the feasibility of using Go and SQLite for managing a personal movie collection. The project lays the groundwork for further enhancements, including a personal movie recommendation system, which could offer advantages over existing solutions like IMDb by providing personalized recommendations.
+
---
+++ src/main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	_ "modernc.org/sqlite"
+)
+
+func main() {
+	if err := main1(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main1() error {
+	dir, err := os.MkdirTemp("", "test-")
+	if err != nil {
+		return err
+	}
+
+	defer os.RemoveAll(dir)
+
+	fn := filepath.Join(dir, "movie_database.db")
+
+	db, err := sql.Open("sqlite", fn)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	// // Create tables
+	// if err := createTables(db); err != nil {
+	// 	return err
+	// }
+
+	// Populate database with movie data
+	if err := populateDatabase(db); err != nil {
+		return err
+	}
+
+	// Query to get a specific row
+	row := db.QueryRow("SELECT * FROM movies WHERE id = 191243")
+	var id int
+	var name string
+	var year int
+	var rank float64
+	err = row.Scan(&id, &name, &year, &rank)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID: %d, Name: %s, Year: %d, Rank: %f\n", id, name, year, rank)
+
+	// Remove quotation marks from name
+	name = strings.ReplaceAll(name, "\"", "")
+
+	// Query to get the total number of movies from 2022
+	row = db.QueryRow("SELECT COUNT(*) FROM movies WHERE year = 1991")
+	var count int
+	err = row.Scan(&count)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Total movies from 1991: %d\n", count)
+
+	return nil
+}
+
+// func createTables(db *sql.DB) error {
+// 	// Create table query
+// 	query := `
+// 	CREATE TABLE IF NOT EXISTS movies (
+// 		id INTEGER PRIMARY KEY,
+// 		name TEXT,
+// 		year INTEGER
+// 	);
+// 	`
+// 	_, err := db.Exec(query)
+// 	return err
+// }
+
+func populateDatabase(db *sql.DB) error {
+	// Create table
+	query := `
+	CREATE TABLE movies (
+		id INTEGER,
+		name TEXT,
+		year INTEGER,
+		rank REAL
+	);
+	`
+	_, err := db.Exec(query)
+	if err != nil {
+		return err
+	}
+
+	// Define file names
+	files := []string{
+		//		"../database/IMDB-directors_genres.csv",
+		//		"../database/IMDB-directors.csv",
+		//		"../database/IMDB-movies_genres.csv",
+		"../database/IMDB-movies.csv",
+		//		"../database/IMDB-roles.csv",
+	}
+
+	// Define queries for each file
+	queries := []string{
+		//		"INSERT INTO directors_genres (director_id, genre, prob) VALUES (?, ?, ?)",
+		//		"INSERT INTO directors (id, first_name, last_name) VALUES (?, ?, ?)",
+		//		"INSERT INTO movies_genres (movie_id, genre) VALUES (?, ?)",
+		"INSERT INTO movies (id, name, year, rank) VALUES (?, ?, ?, ?)",
+		//		"INSERT INTO roles (actor_id, movie_id, role) VALUES (?, ?, ?)",
+	}
+
+	// Loop over files
+	for i, file := range files {
+		// Open file
+		f, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		// Create CSV reader
+		r := csv.NewReader(f)
+
+		// Loop over rows
+		for {
+			record, err := r.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Printf("CSV parsing error on line: %v", err)
+				continue
+			}
+
+			// Convert []string to []interface{}
+			recordInterface := make([]interface{}, len(record))
+			for i, v := range record {
+				// Remove quotation marks from name field
+				if i == 1 {
+					v = strings.ReplaceAll(v, "\"", "")
+				}
+				// Convert "NULL" to nil
+				if v == "NULL" {
+					recordInterface[i] = 0
+				} else {
+					recordInterface[i] = v
+				}
+			}
+
+			// Insert data into database
+			_, err = db.Exec(queries[i], recordInterface...)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
